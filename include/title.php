ce52c22ad9daae734515f2a1139ef484920d4e66
jessie <jessielin0422@gmail.com>
2026-06-08 09:33:27 +0800

    建立新增標題圖片的後端api及資料表

--- include/title.php
+++ include/title.php
@@ -13,3 +13,7 @@
     </table>
     <div class="cent"><input type="submit" value="新增"><input type="reset" value="重置"></div>
     </form>
+
+<!-- <form action="api/add_title.php" method="post" enctype="multipart/form-data"> 
+    新增介面的功能:連結的檔案的位置 
+    依照不同功能的轉變要變更資料的內容位置-->
