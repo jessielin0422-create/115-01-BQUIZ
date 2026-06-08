5d53194a4b095455ba5501b3ca825325ee206f8d
jessie <jessielin0422@gmail.com>
2026-06-08 09:29:38 +0800

    建立新增標題圖片的後端api及資料表

---
+++ api/api_title.php
@@ -0,0 +1,17 @@
+<?php
+include_once "db.php";
+
+if(!empty($_FILES['img']['tmp_name'])){
+     move_uploaded_file($_FILES['img']['tmp_name'],"../upload/".$_FILES['img']['name']);
+     $_POST['TEXT'];
+     $_POST['img']=$_FILES['img']['name'];
+     $_POST['sh']=0;
+     $Title->save($_POST);
+}
+
+to("../admin.php?do=title");
+
+?>
+
+<!-- mysql的新增中間連結 -->
+<!-- ../ 連線代表符號的意思 -->
