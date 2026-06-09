d4ea3db388511e770d59f9c0954b603ec8dc8d66
jessie <jessielin0422@gmail.com>
2026-06-09 09:22:05 +0800

    整合多支edit api為一支,並修改後台的資料列表讀取方式

--- api/edit_ad.php
+++ api/edit_ad.php
@@ -1,22 +1,44 @@
 <?php
 include_once "db.php";
 
+$table=$_GET['table'];
+$db=${ucfirst($table)};
+
 foreach($_POST['id'] as $idx => $id){
      if(isset($_POST['del']) && in_array($id,$_POST['del'])){
           $$Ad->del($id);
      }else{
 
-      $row=$$Ad->find($id);
+      $row=$$db->find($id);
+
+     switch($table){
+     case "title":
         $row['text']=$_POST['text'][$idx];
         $row['sh']=(isset($_POST['sh']) && in_array($id,$_POST['sh']))?1:0;
+     break;
+     case "mvim":
+     case "image":
+       $row['sh']=(isset($_POST['sh']) && in_array($id,$_POST['sh']))?1:0;
+     break;
+     case "admin":
+          $row['acc']=$_POST['acc']['$idx'];
+          $row['pw']=$_POST['pw']['$idx'];
+     break;
+     case "menu":
+          $row['href']=$_POST['href'][$idx];
+          $row['text']=$_POST['text'][$idx];
+          $row['sh']=(isset($_POST['sh']) && in_array($id,$_POST['sh']))?1:0;
+     break;
+      
+       
 
 
-          $$Ad->save($row);
+          $$db->save($row);
      }
 
 }
 
-to("../admin.php?do=ad");
+to("../admin.php?do=$table");
 
 ?>
 
