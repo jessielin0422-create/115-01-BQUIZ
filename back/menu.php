663aa4b363dd2c0a3ef2b30d8c6e638d53e67031
jessie <jessielin0422@gmail.com>
2026-06-09 16:01:58 +0800

    完成編輯次選單頁面及增/改/刪功能

--- back/menu.php
+++ back/menu.php
@@ -1,4 +1,3 @@
-
 <div class="di"
     style="height:540px; border:#999 1px solid; width:76.5%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
     <!--正中央-->
@@ -16,33 +15,45 @@
         </tbody>
     </table>
     <div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
-        <p class="t cent botli">管理者帳號管理</p>
-        <form method="post" action="./api/edit_ad.php">
+        <p class="t cent botli">選單管理</p>
+        <form method="post" action="./api/edit.php?table=<?= $do ?>">
             <table width="100%">
                 <tbody>
                     <tr class="yel">
-                        <td width="45%">帳號</td>
-                        <td width="45%">密碼</td>
+                        <td width="30%">主選單名稱</td>
+                        <td width="30%">選單連結網址</td>
+                        <td width="10%">次選單數</td>
+                        <td width="10%">顯示</td>
                         <td width="10%">刪除</td>
+                        <td width="10%"></td>
                     </tr>
-                    <?php 
-                    $db=${ucfirst($do)};
-                    $row=$db->all();
-                    foreach($row as $row):
+                    <?php
+                    $db = ${ucfirst($do)};
+                    $row = $db->all(['main_id' => 0]);
+                    foreach ($row as $row):
                     ?>
-                    
-                    <tr>
-                        <td width="45%">
-                            <input type="text" name="acc[]" value="<?= $row['acc']; ?>" style="width:95%">
-                        </td>
-                        <td width="45%">
-                            <input type="password" name="pw[]" value="<?= $row['pw']; ?>"> 
-                        </td>
-                        <td width="10%">
-                            <input type="checkbox" name="del[]" value="<?= ['id']; ?>">
-                        </td>
-                        <input type="hidden" name="id[]" value="<?= $row['id']; ?>">
-                    </tr>
+
+                        <tr>
+                            <td>
+                                <input type="text" name="text[]" value="<?= $row['text']; ?>" style="width:95%">
+                            </td>
+                            <td>
+                                <input type="text" name="href[]" value="<?= $row['href']; ?>">
+                            </td>
+                            <td>
+                            </td>
+                            <td>
+                                <input type="checkbox" name="sh[]" value="<?= $$row['id']; ?>" <?=($row['sh']==1)?'checked':''; ?> >
+                            </td>
+                             <td>
+                                <input type="checkbox" name="del[]" value="<?= $row['id']; ?>">
+                            </td>
+                            <td>
+                                <input type="button" value="編輯次選單" onclick="op('#cover','#cvr','include/submenu.php?id=<?= $row['id'];?>')">                                                              
+                            </td>
+
+                            <input type="hidden" name="id[]" value="<?= $row['id']; ?>">
+                        </tr>
                     <?php
                     endforeach;
                     ?>
@@ -52,7 +63,7 @@
                 <tbody>
                     <tr>
                         <td width="200px">
-                            <input type="button" onclick="op('#cover','#cvr','include/<?= $do; ?>.php')" value="新增管理者帳號">
+                            <input type="button" onclick="op('#cover','#cvr','include/<?= $do; ?>.php')" value="新增主選單">
                         </td>
                         <td class="cent">
                             <input type="submit" value="修改確定">
