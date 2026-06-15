b3e21a22461f148c8fe3923ec0fcbe53bcb2c188
jessie <jessielin0422@gmail.com>
2026-06-15 11:00:24 +0800

    完成顯示五筆最新消息資料及彈出詳細內容

--- front/main.php
+++ front/main.php
@@ -1,6 +1,6 @@
  <div class="di" style="height:540px; border:#999 1px solid; width:53.2%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
 
-      <?php include "include/marquee.php"; ?>
+      <?php include "include/marquee.php";?>
 
       <div style="height:32px; display:block;"></div>
       <!--正中央-->
@@ -41,6 +41,14 @@
            <span class="t botli">最新消息區
            </span>
            <ul class="ssaa" style="list-style-type:decimal;">
+               <?php 
+                            $news=$News->all(['sh'=>1]," limit 5");
+                            foreach($news as $n):
+                            ?>
+                            <li><?= mb_substr($n['text'],0,25); ?>
+                            <div class="all" style="display:none"><?= $n['text']; ?></div>
+                            </li>
+                            <?php endforeach;?>
            </ul>
            <div id="altt" style="position: absolute; width: 350px; min-height: 100px; background-color: rgb(255, 255, 204); top: 50px; left: 130px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;"></div>
            <script>
