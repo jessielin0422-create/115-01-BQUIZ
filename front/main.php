3166ccfa4d28c8a523f20517e3304d45838a948b
jessie <jessielin0422@gmail.com>
2026-06-15 11:19:23 +0800

    完成首頁更多最新消息的判斷及more連結的顯示

--- front/main.php
+++ front/main.php
@@ -39,16 +39,22 @@
 
       <div style="width:95%; padding:2px; height:190px; margin-top:10px; padding:5px 10px 5px 10px; border:#0C3 dashed 3px; position:relative;">
            <span class="t botli">最新消息區
+               <a href="?do=news" style="float:right">
+               <?php
+               if($News->count(['sh=>1'])>5){
+                    echo "Move...";
+               }
+               ?>     
            </span>
            <ul class="ssaa" style="list-style-type:decimal;">
-               <?php 
-                            $news=$News->all(['sh'=>1]," limit 5");
-                            foreach($news as $n):
-                            ?>
-                            <li><?= mb_substr($n['text'],0,25); ?>
-                            <div class="all" style="display:none"><?= $n['text']; ?></div>
-                            </li>
-                            <?php endforeach;?>
+                <?php
+                    $news = $News->all(['sh' => 1], " limit 5");
+                    foreach ($news as $n):
+                    ?>
+                     <li><?= mb_substr($n['text'], 0, 25); ?>
+                          <div class="all" style="display:none"><?= $n['text']; ?></div>
+                     </li>
+                <?php endforeach; ?>
            </ul>
            <div id="altt" style="position: absolute; width: 350px; min-height: 100px; background-color: rgb(255, 255, 204); top: 50px; left: 130px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;"></div>
            <script>
