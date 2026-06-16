9267a61f49ab3de1da86e9f5eb2c7517cf998d29
jessie <jessielin0422@gmail.com>
2026-06-16 08:57:21 +0800

    依題意完成管理登入功能

--- front/login.php
+++ front/login.php
@@ -1,4 +1,20 @@
-                <div class="di" style="height:540px; border:#999 1px solid; width:53.2%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
+               <?php
+			if(isset($_POST['acc'])){
+				if($_POST['acc']=='admin' && $_POST['pw']=='1234'){
+					$_SESSION['login']=1;
+					to("admin.php");
+				}else{
+					echo "<script>";
+					echo "alert('帳號或密碼錯誤')";
+					echo "</script>";
+				}
+			}
+			
+			?>
+			
+			
+			
+			<div class="di" style="height:540px; border:#999 1px solid; width:53.2%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
                 	<?php include "include/marquee.php"; ?>
                 	<div style="height:32px; display:block;"></div>
                 	<!--正中央-->
